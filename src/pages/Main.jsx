4d99d9837bfd32fc7002c373db4dad8fa8136915
AkaiBekjanov <bekjanov.akai.official@gmail.com>
2023-12-04 23:06:10 +0600

    added pagination,changed api

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,22 +4,38 @@ import styles from "./styles.module.css";
 import { getNews } from "../api/api";
 import { NewsList } from "../components/NewsList/NewsList";
 import { Skeleton } from "../components/Skeleton/Skeleton";
+import { Pagination } from "./../components/Pagination/Pagination";
 
 export const Main = () => {
   const [news, setNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState([]);
   const [isLoading, setLoading] = useState(true);
+
+  const totalPages = 10;
+  var pageSize = 10;
+  const fetchNews = async (currentPage) => {
+    try {
+      const res = await getNews(currentPage, pageSize);
+      setNews(res.news);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const res = await getNews();
-        setNews(res.news);
-        setLoading(false);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchNews();
-  }, []);
+    fetchNews(currentPage);
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
   return (
     <div className={styles.main}>
       {news.length > 0 && !isLoading ? (
@@ -27,6 +43,13 @@ export const Main = () => {
       ) : (
         <Skeleton count={1} type={"banner"} />
       )}
+      <Pagination
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+        handleNextPage={handleNextPage}
+        handlePrevPage={handlePrevPage}
+        currentPage={currentPage}
+      />
       {!isLoading ? (
         <NewsList news={news} />
       ) : (
